d9c56f3110f50372338e3614f0076ed03585ff3d
Melike Öğmen <76531681+Melike916@users.noreply.github.com>
2020-12-25 02:56:41 +0300

    Add files via upload

---
+++ Ödev1.py
@@ -0,0 +1,6 @@
+data1 = input("Please enter a value: ")
+data2 = input("Please enter a value: ")
+data3 = input("Please enter a value: ")
+data4 = input("Please enter a value: ")
+data5 = input("Please enter a value: ")
+print("data1:{}, data2:{}, data3:{}, data4:{}, data5:{}".format(data1, data2, data3, data4, data5))
