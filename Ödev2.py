05e6fcac6e764a4506b7400f7de3654ad927c96b
Melike Öğmen <76531681+Melike916@users.noreply.github.com>
2020-12-25 03:00:49 +0300

    Add files via upload

---
+++ Ödev2.py
@@ -0,0 +1,10 @@
+first_name= input("Please enter your first name: ")
+last_name= input("Please enter your last name: ")
+age= int(float(input("Please enter your age: ")))
+date_of_birt= int(float(input("Please enter your date of birt (just year): ")))
+mylist= [age]
+for x in mylist:
+  if age < 18:
+    print("You can't go out because it's too dangerous!!)
+  else:
+    print("You can go out to the street.")
